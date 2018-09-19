f351e653cb6abd5ec5b99e546e1d3b7c78448929
Samuel Prada <jspradac@gmail.com>
2018-09-19 22:54:12 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,4 +1,10 @@
 // declare your functions here...
 function paragraphSelector() {
-  return $("p")
+  return $("p");
+}
+function lastImageSelector() {
+  return $("img:last");
+}
+function ninjaBabySelector() {
+  return $("")
 }
